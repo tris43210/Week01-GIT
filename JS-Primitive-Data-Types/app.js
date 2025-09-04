c82090a5b647a379925f206acbdcc54c60a2fa25
tris43210 <tristan.g.king@gmail.com>
2025-09-04 11:20:39 +0100

    Arithmitic Operations and Comparisions with Primitives

---
+++ JS-Primitive-Data-Types/app.js
@@ -0,0 +1,19 @@
+// basic arthmitic with variables
+const sweets = 20;
+const friends = 6;
+const sweetsPerFriend = 20 / 6;
+console.log(`Each Friend Gets ${sweetsPerFriend} sweets`);
+
+// Boolean Values
+const isTrue = true;
+const isFalse = false;
+console.log(`The value of isTrue is ${isTrue}`);
+console.log(`The value of isFalse is ${isFalse}`);
+
+// Comparison Operators with if...else
+
+if (sweets > friends) {
+  console.log(`there are more sweets than friends`);
+} else {
+  console.log(`We need More Sweets!`);
+}
