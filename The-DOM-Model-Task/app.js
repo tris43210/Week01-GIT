fd900711abb92d78f1f3507044a8825c053c65ce
tris43210 <tristan.g.king@gmail.com>
2025-09-04 19:20:05 +0100

    Started DOM Model Task

---
+++ The-DOM-Model-Task/app.js
@@ -0,0 +1,8 @@
+const HTMLbody = document.querySelector("body");
+const h1 = document.querySelector("h1");
+const img = document.querySelector("img");
+
+console.log(HTMLbody);
+console.log(h1);
+h1.innerText = `New Javascript Text`;
+img.src = "images/440603160_1080713336356287_586683118111036387_n.jpg";
