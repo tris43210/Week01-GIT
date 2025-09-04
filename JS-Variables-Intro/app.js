4508e5eac9427c662bce4581145f593ce550db18
tris43210 <tristan.g.king@gmail.com>
2025-09-04 10:40:45 +0100

    changing Variable Values and Using Template Literals to Concatinate Strings

---
+++ JS-Variables-Intro/app.js
@@ -0,0 +1,11 @@
+const myName = "Tristan";
+const age = 26;
+
+// string concatination with template literals
+console.log(`Hello My Name is ${myName} and I and ${age} years old`);
+console.log(`My age in the next 10 years will be ${age + 10}`);
+
+// resassinging values using let variables
+let oldmanTK = null;
+console.log(`Tristan at an old age will be ${(oldmanTK = 56)}`);
+console.log(oldmanTK);
